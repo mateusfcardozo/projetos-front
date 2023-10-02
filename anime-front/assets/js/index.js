99f3c150305eb215b95dae28ff8123988a6e2dd9
mateus <mateusfcardozo245@gmail.com>
2023-10-02 19:25:19 -0300

    .

---
+++ anime-front/assets/js/index.js
@@ -0,0 +1,5 @@
+const botoes= document.querySelectorAll('.botao');
+const botao =document.getElementById('Gojo');
+document.getElementById('Itadori');
+document.getElementById('Nobara');
+document.getElementById('Megumi');
